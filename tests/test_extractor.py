333095547a7b5d392e01a37c833a219ce72281a6
julio Almeida <enoch3712@gmail.com>
2024-11-12 17:59:33 +0100

    removing direct path to local files

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,8 +1,6 @@
 import asyncio
 import os
-import sys
 import time
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from dotenv import load_dotenv
 from extract_thinker.extractor import Extractor
 from extract_thinker.document_loader.document_loader_tesseract import DocumentLoaderTesseract
