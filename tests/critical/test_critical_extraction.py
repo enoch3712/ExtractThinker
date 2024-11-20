cde8c2a873f34841df265a30ee606d55fe18cadd
julio Almeida <enoch3712@gmail.com>
2024-11-20 12:32:49 +0100

    remove hardcoded paths to the local files

--- tests/critical/test_critical_extraction.py
+++ tests/critical/test_critical_extraction.py
@@ -1,7 +1,4 @@
 import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
 from dotenv import load_dotenv
 from extract_thinker import Extractor
 from extract_thinker.document_loader.document_loader_pypdf import DocumentLoaderPyPdf
