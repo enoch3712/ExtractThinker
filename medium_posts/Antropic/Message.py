76d5698aa09993e19ca8a5e875bc71bdaabb7ee7
julio Almeida <enoch3712@gmail.com>
2024-12-16 14:07:42 +0100

    folder removed

--- medium_posts/Antropic/Message.py
+++
@@ -1,4 +0,0 @@
-class Message:
-    def __init__(self, role: str, content: str):
-        self.role = role
-        self.content = content
--- medium_posts/Classification/Eval.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Eval(ABC):
-    def __init__(self, threshold: float):
-        self.threshold = threshold
-
-    @abstractmethod
-    def evaluate(self, result):
-        pass
