d502b68e974389a687f5adff1b79198d58fc7f9f
julio Almeida <enoch3712@gmail.com>
2025-04-02 01:59:52 +0100

    missing configs added

--- extract_thinker/__init__.py
+++ extract_thinker/__init__.py
@@ -3,14 +3,14 @@ from .extractor import Extractor
 from .llm import LLM
 from .document_loader.document_loader import DocumentLoader
 from .document_loader.cached_document_loader import CachedDocumentLoader
-from .document_loader.document_loader_tesseract import DocumentLoaderTesseract
+from .document_loader.document_loader_tesseract import DocumentLoaderTesseract, TesseractConfig
 from .document_loader.document_loader_spreadsheet import DocumentLoaderSpreadSheet
-from .document_loader.document_loader_azure_document_intelligence import DocumentLoaderAzureForm
+from .document_loader.document_loader_azure_document_intelligence import DocumentLoaderAzureForm, AzureConfig
 from .document_loader.document_loader_pypdf import DocumentLoaderPyPdf, PyPDFConfig
-from .document_loader.document_loader_pdfplumber import DocumentLoaderPdfPlumber
-from .document_loader.document_loader_beautiful_soup import DocumentLoaderBeautifulSoup
-from .document_loader.document_loader_markitdown import DocumentLoaderMarkItDown
-from .document_loader.document_loader_docling import DocumentLoaderDocling
+from .document_loader.document_loader_pdfplumber import DocumentLoaderPdfPlumber, PDFPlumberConfig
+from .document_loader.document_loader_beautiful_soup import DocumentLoaderBeautifulSoup, BeautifulSoupConfig
+from .document_loader.document_loader_markitdown import DocumentLoaderMarkItDown, MarkItDownConfig
+from .document_loader.document_loader_docling import DocumentLoaderDocling, DoclingConfig
 from .models.classification import Classification
 from .models.classification_response import ClassificationResponse
 from .process import Process
@@ -21,14 +21,15 @@ from .models.contract import Contract
 from .models.splitting_strategy import SplittingStrategy
 from .models.completion_strategy import CompletionStrategy
 from .batch_job import BatchJob
-from .document_loader.document_loader_txt import DocumentLoaderTxt
-from .document_loader.document_loader_doc2txt import DocumentLoaderDoc2txt
-from .document_loader.document_loader_aws_textract import DocumentLoaderAWSTextract
-from .document_loader.document_loader_llm_image import DocumentLoaderLLMImage
-from .document_loader.document_loader_data import DocumentLoaderData
+from .document_loader.document_loader_txt import DocumentLoaderTxt, TxtConfig
+from .document_loader.document_loader_doc2txt import DocumentLoaderDoc2txt, Doc2txtConfig
+from .document_loader.document_loader_aws_textract import DocumentLoaderAWSTextract, TextractConfig
+from .document_loader.document_loader_llm_image import DocumentLoaderLLMImage, LLMImageConfig
+from .document_loader.document_loader_data import DocumentLoaderData, DataLoaderConfig
 from .document_loader.document_loader_google_document_ai import (
     DocumentLoaderGoogleDocumentAI,
     DocumentLoaderDocumentAI,
+    GoogleDocAIConfig
 )
 from .warning import filter_pydantic_v2_warnings
 from .document_loader.document_loader_mistral_ocr import DocumentLoaderMistralOCR, MistralOCRConfig
@@ -40,23 +41,35 @@ __all__ = [
     'DocumentLoader',
     'CachedDocumentLoader',
     'DocumentLoaderTesseract',
+    'TesseractConfig',
     'DocumentLoaderSpreadSheet',
     'DocumentLoaderAzureForm',
+    'AzureConfig',
     'DocumentLoaderPyPdf',
     'PyPDFConfig',
     'DocumentLoaderPdfPlumber',
+    'PDFPlumberConfig',
     'DocumentLoaderBeautifulSoup',
+    'BeautifulSoupConfig',
     'DocumentLoaderLLMImage',
+    'LLMImageConfig',
     'DocumentLoaderTxt',
+    'TxtConfig',
     'DocumentLoaderDoc2txt',
+    'Doc2txtConfig',
     'DocumentLoaderAWSTextract',
+    'TextractConfig',
     'DocumentLoaderGoogleDocumentAI',
     'DocumentLoaderDocumentAI',
-	'DocumentLoaderMarkItDown',
-	'DocumentLoaderData',
+    'GoogleDocAIConfig',
+    'DocumentLoaderMarkItDown',
+    'MarkItDownConfig',
+    'DocumentLoaderData',
+    'DataLoaderConfig',
     'Classification',
     'CompletionStrategy',
     'DocumentLoaderDocling',
+    'DoclingConfig',
     'ClassificationResponse',
     'Process',
     'ClassificationStrategy',
@@ -65,7 +78,7 @@ __all__ = [
     'TextSplitter',
     'Contract',
     'SplittingStrategy',
-	'BatchJob',
+    'BatchJob',
     'DocumentLoaderMistralOCR',
     'MistralOCRConfig',
 ]
